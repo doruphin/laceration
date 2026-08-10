cf959ca3351aea1347c882293f2e98f6eaaf5e1b
Dorson Tang <apersonnameddorson@gmail.com>
2026-08-09 18:46:54 -0700

    stylistic

--- scripts/GameManager.gd
+++ scripts/GameManager.gd
@@ -1,8 +1,10 @@
 extends Node
 
-signal stabbed_location(location: String)
+signal stabbed_location(location: String, force: float)
 signal points_changed(points: int)
 signal lost_game
+
+const BASE_POINTS = 10
 
 # global vars
 var playing = true
@@ -13,6 +15,7 @@ const MAX_FINGERS = 5
 var next_finger = 1
 var start_stabbed = false
 var points = 0
+var order_combo = 1
 
 # scoring logic
 var round_timer = Timer.new()
@@ -23,7 +26,8 @@ func _ready():
 	stabbed_location.connect(_on_stabbed_location)
 	points_changed.connect(_on_points_changed)
 
-func _on_stabbed_location(location: String):
+func _on_stabbed_location(location: String, force: float):
+	var just_started = false
 	match location:
 		"Knife":
 			pass
@@ -33,15 +37,21 @@ func _on_stabbed_location(location: String):
 			lost_game.emit()
 		"StartZone":
 			start_stabbed = true
+			just_started = true
 		_:
 			if (start_stabbed and str(next_finger) == location):
 				# happy path, we are in order
 				next_finger = next_finger + 1 if next_finger != MAX_FINGERS else 1
 				start_stabbed = false
-				points_changed.emit(points+10)
+				points_changed.emit(points+BASE_POINTS*min(order_combo, 8)*force)
+				order_combo += 1
 			else:
 				# wrong order
-				lost_game.emit()
+				points_changed.emit(points+BASE_POINTS*force)
+				order_combo = 1
+				next_finger = 1
+				if !just_started:
+					start_stabbed = false
 
 func _on_points_changed(new_points: int):
 	points = new_points
--- scripts/Knife.gd
+++ scripts/Knife.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 var prev_pos = Vector2()
+var stuck_pos = Vector2()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void: 
@@ -8,25 +9,27 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	global_position = prev_pos if GameManager.knife_stuck else get_global_mouse_position()
+	prev_pos = get_global_mouse_position()
+	global_position = stuck_pos if GameManager.knife_stuck else prev_pos
 
+# table is kind of misleading, right now it dictates anything stabbable
+# as a byproduct, anything stabbable needs to be in the table hitbox
+# not sure how to change that, we could also just accept it
 func _on_table_mouse_entered() -> void:
 	GameManager.knife_stuck = true
-	prev_pos = get_global_mouse_position()
+	stuck_pos = get_global_mouse_position()
 	
 	# what did we just stab
 	var space_state = get_world_2d().direct_space_state
 	var query = PhysicsPointQueryParameters2D.new()
-	query.position = prev_pos
+	query.position = stuck_pos
 	query.collide_with_areas = true
 	var res = space_state.intersect_point(query)
-	
-	# branch off what we just stabbed
 	for result in res:
-		GameManager.stabbed_location.emit(result.collider.name)
+		GameManager.stabbed_location.emit(result.collider.name, Vector2(prev_pos - stuck_pos).length())
 
 func _on_table_mouse_exited() -> void:
 	GameManager.knife_stuck = false
 	
-	var viewport_pos: Vector2 = get_viewport().get_canvas_transform() * prev_pos
+	var viewport_pos: Vector2 = get_viewport().get_canvas_transform() * stuck_pos
 	get_viewport().warp_mouse(viewport_pos)
